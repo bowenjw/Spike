4e1bae0056645cbd5de90e6cd1eaac1cb6121387
John WIlliam Bowen <johnwb1776@gmail.com>
2022-10-08 00:26:43 +0000

    Removed client.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
 import fs from 'fs';
 import mongoose from 'mongoose';
-import { client } from './client';
+import {Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
 // Conect to the Data Base
 // mongoose.connect(process.env.MONGO_URI!,{keepAlive:true})
 //	.then(() => console.log('Conected to DB\n'))
 //	.catch((err) => console.log(err));
-	
+
+dotenv.config();
+// Create a new client instance
+export const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildVoiceStates,
+    ],
+}),
+	token = process.env.DISCORD_TOKEN!,
+	applicationID = process.env.DISCORD_APPLICATION_ID!;
+
 // Event file location
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.ts'));
 
@@ -20,3 +33,6 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
+// Login to Discord with your client's token
+client.login(token);
