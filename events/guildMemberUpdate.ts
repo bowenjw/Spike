311d89f30e1ac887b109aaf8e21c7a05f70c0a13
John WIlliam Bowen <johnwb1776@gmail.com>
2022-10-08 00:28:05 +0000

    aaded audit log logging

---
+++ events/guildMemberUpdate.ts
@@ -0,0 +1,37 @@
+import { AuditLogEvent, EmbedBuilder, GuildMember, TextChannel } from 'discord.js';
+import { Event } from '../types'
+
+const event: Event = {
+	name: 'guildMemberUpdate',
+    once: false,
+	async execute(oldMember: GuildMember, newMember: GuildMember) {
+		const guild = newMember.guild,
+			logChannel = await guild.channels.fetch('970498463953453179') as TextChannel,
+			entry = (await guild.fetchAuditLogs({limit: 1, type: AuditLogEvent.MemberDisconnect || AuditLogEvent.MemberUpdate})).entries.first()!,
+			executor = await guild.members.fetch(entry.executor?.id!);
+
+		let embed = new EmbedBuilder()
+				.setFooter({text: `Action by ${executor.displayName}`, iconURL: executor.displayAvatarURL() })
+				.setThumbnail(newMember.displayAvatarURL())
+				.setColor('#fffa77')
+				.setTimestamp();
+		console.log(entry)
+
+		if(!oldMember.communicationDisabledUntilTimestamp && newMember.communicationDisabledUntilTimestamp){
+			const endedDate = Math.floor(newMember.communicationDisabledUntil!.getTime()/1000)
+			embed = embed.setTitle('Timeout')
+			.setDescription(`Member ${newMember}(${newMember.displayName}) has been timed out`)
+			.addFields(
+				{name: 'Reason', value: entry.reason!, inline: false},
+				{name: 'Timed Out Until', value: `<t:${endedDate}:R>\n <t:${endedDate}:F>`})
+			logChannel.send({embeds: [embed]})
+		}
+		else if(oldMember.communicationDisabledUntilTimestamp && !newMember.communicationDisabledUntilTimestamp){
+			embed = embed.setTitle('Timeout Ended Early')
+				.setDescription(`Member ${newMember} had their timeout ended early`)
+			logChannel.send({embeds: [embed]})
+		}
+	}
+};
+
+export = event;
