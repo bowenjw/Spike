24cfd32e238aab2fef46bccf761e9d478ea1d507
John William Bowen <johnwb1776@gmail.com>
2022-10-10 09:22:15 -0400

    fixed log

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -10,13 +10,12 @@ const event: Event = {
 
 		// Command interaction
 		try {
-			console.log(`${interaction.user.tag} used Button from ${interaction.guild?.name} in channel ${(interaction.channel as GuildChannel).name}`);
+			console.log(`${interaction.user.tag} used ${interaction.type} from ${interaction.guild?.name} in channel ${(interaction.channel as GuildChannel).name}`);
 			runApplicationCommand(interaction);
 		} catch (error: unknown) {
 			console.error(error);
 			if((interaction as CommandInteraction).deferred){
 				(interaction as CommandInteraction).followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-
 			} else
 				(interaction as CommandInteraction).reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
