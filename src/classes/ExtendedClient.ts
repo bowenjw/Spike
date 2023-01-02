636abad60ce026374b232e46d2dc75e88d9e6bbc
John William Bowen <johnwb1776@gmail.com>
2023-01-01 23:27:36 -0500

    removed deploy.ts and updated ExtendedClient.ts

--- src/classes/ExtendedClient.ts
+++ src/classes/ExtendedClient.ts
@@ -1,6 +1,6 @@
-import { Client, Collection } from 'discord.js';
+import { ApplicationCommand, Client, ClientOptions, Collection, REST, RESTPostAPIApplicationCommandsJSONBody, Routes } from 'discord.js';
 import { Icommand, Ievent, Iinteraction } from '../interfaces';
-//import configJSON from '../config.json';
+// import configJSON from '../config.json';
 import path from 'path';
 import { readdirSync } from 'fs';
 
@@ -14,56 +14,70 @@ try {
 } catch (e) { }
 
 class ExtendedClient extends Client {
-    public commands: Collection<string, Icommand> = new Collection();
-    public contextMenus : Collection<string, Icommand> = new Collection();
-    public events: Collection<string, Event> = new Collection();
-    public interactions: Collection<string, Iinteraction> = new Collection();
+    readonly commands: Collection<string, Icommand> = new Collection();
+    readonly contextMenus : Collection<string, Icommand> = new Collection();
+    readonly events: Collection<string, Ievent> = new Collection();
+    readonly buttons: Collection<string, Iinteraction> = new Collection();
+    readonly selectMenus: Collection<string, Iinteraction> = new Collection();
+    readonly modals: Collection<string, Iinteraction> = new Collection();
+
 
     readonly eventPath = path.join(__dirname, '..', 'events')
-    readonly interactionPath = path.join(__dirname, '..', 'interactions')
     readonly commandPath = path.join(__dirname, '..', 'commands', 'chat')
-    readonly contextMenuPath = path.join(this.commandPath, '..', 'context_menu')
-    readonly RESTVersion = '10'
-
-    public async init() {
-
+    readonly contextMenuPath = path.join(__dirname, '..', 'commands', 'context_menu')
+    readonly buttonPath = path.join(__dirname, '..', 'interactions', 'button')
+    readonly selectMenuPath = path.join(__dirname, '..', 'interactions', 'select_menu')
+    readonly modalPath = path.join(__dirname, '..', 'interactions', 'modal')
+    
+    constructor(options: ClientOptions) {
+        super(options)
         console.log('Starting up...')
-        
+        const commands: RESTPostAPIApplicationCommandsJSONBody[] = []
+
         // Command Handler
         readdirSync(this.commandPath).filter((file) => file.endsWith(tsNodeRun ? '.ts' : '.js')).forEach((file) => {
-        // console.log(path.join(this.commandPath, file))    
-        import(path.join(this.commandPath, file))
-            .then((command:Icommand) => {
-                // console.log(command)
+            import(path.join(this.commandPath, file)).then((command:Icommand) => {
+                commands.push(command.builder.toJSON())
                 this.commands.set(command.builder.name, command)
-            })
-        })
-
-        // context Menus Handler
-        // console.log(readdirSync(this.contextMenuPath))
-        readdirSync(this.contextMenuPath).filter((file) => file.endsWith(tsNodeRun ? '.ts' : '.js')).forEach((file) => 
-            import(path.join(this.contextMenuPath, file))
-            .then((command:Icommand) => {
-                // console.log(command.builder.name)
-                this.contextMenus.set(command.builder.name, command)
-            })
-        )
-
-        // Interaction Handler
-        readdirSync(this.interactionPath).forEach((dir) => {
-            readdirSync(path.join(this.interactionPath, dir)).filter((file) => file.endsWith(tsNodeRun ? '.ts' : '.js'))
-            .forEach((file) => {
-                import(path.join(this.interactionPath, dir, file))
-                .then((interaction:Iinteraction) => {
-                    // console.log(interaction)
-                    this.interactions.set(interaction.name, interaction)})
-            })
+            });
         });
 
+        // context Menus Handler
+        readdirSync(this.contextMenuPath).filter((file) => file.endsWith(tsNodeRun ? '.ts' : '.js')).forEach((file) => {
+            import(path.join(this.contextMenuPath, file)).then((command:Icommand) => {
+                this.contextMenus.set(command.builder.name, command)
+                commands.push(command.builder.toJSON()) 
+            });
+        });
+
+        // Button Handler
+        readdirSync(this.buttonPath).filter((file) => file.endsWith(tsNodeRun ? '.ts' : '.js')).forEach((file) => {
+            import(path.join(this.buttonPath, file)).then((interaction:Iinteraction) => {
+                this.buttons.set(interaction.name, interaction) 
+            });
+        });
+
+        // Select Menu Handler
+        readdirSync(this.buttonPath).filter((file) => file.endsWith(tsNodeRun ? '.ts' : '.js')).forEach((file) => {
+            import(path.join(this.buttonPath, file)).then((interaction:Iinteraction) => {
+                this.selectMenus.set(interaction.name, interaction) 
+            });
+        });
+
+        // Modal Handler
+        readdirSync(this.modalPath).filter((file) => file.endsWith(tsNodeRun ? '.ts' : '.js')).forEach((file) => {
+            import(path.join(this.modalPath, file)).then((interaction:Iinteraction) => {
+                this.modals.set(interaction.name, interaction) 
+            });
+        });
+
+
         // Event Handler
-        readdirSync(this.eventPath).filter((dir) => dir.endsWith(tsNodeRun ? '.ts' : '.js'))
-        .forEach((file) => import(path.join(this.eventPath, file))
+        readdirSync(this.eventPath).filter((dir) => dir.endsWith(tsNodeRun ? '.ts' : '.js')).forEach((file) => import(path.join(this.eventPath, file))
             .then((event:Ievent) => {
+
+                this.events.set(event.name, event);
+
                 if (event.once) 
                     this.once(event.name, (...args) => event.execute(...args))
                 else
@@ -72,8 +86,21 @@ class ExtendedClient extends Client {
         )
 
         // Login
-        if (!process.env.DISCORD_TOKEN) return console.error('No token was specified. Did you create a .env file?');
+        if (!process.env.DISCORD_TOKEN) console.error('No token was specified. Did you create a .env file?');
         this.login(process.env.DISCORD_TOKEN);
+
+        // Skip if no-deployment flag is set
+        if (process.argv.includes('--no-deployment')) return;
+        
+        // Deploying Commands
+        async () => {
+            const rest = new REST({ version: '10' }).setToken(this.token!)
+            const applicationCommands = await rest.put(Routes.applicationCommands(this.user!.id), { body: commands })
+                .catch(console.error) as ApplicationCommand[];
+        
+            console.log(`Deployed ${applicationCommands.length} global commands`);
+        }
+        
     }
 }
 
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,11 +1,8 @@
 import { Client, Events } from 'discord.js';
-import { client as bot } from '../bot';
-import { deploy } from '../features';
 
 export const name = Events.ClientReady,
     once = true
 export function execute(client: Client) {
-    deploy(bot);
     console.log(`Ready! Logged in as ${client.user!.tag}`)
 }
 
